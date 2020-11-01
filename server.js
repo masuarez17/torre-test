9a7789d7f44b0e2b299a0797306f917539feb9a3
Marcos Suarez <suarez0550@gmail.com>
2020-11-01 13:43:35 -0500

    added get route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 const port = process.env.port || 8080
 
 app.listen(port)
