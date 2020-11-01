87b5e95f7af5d6e6e5e2e4914a57e406143f963e
Marcos Suarez <suarez0550@gmail.com>
2020-10-31 19:08:29 -0500

    finished the base interface of the main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(VueRouter)
 
@@ -9,14 +11,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid bg-success h-100 p-3">
+    <div class="row align-items-start justify-content-center">
+      <div class="card w-75 p-4 border border-dark">
+        <div class="card-body">
+          <div class="input-group input-group-lg">
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <span>u</span>
+              </div>
+            </div>
+            <input
+              type="text"
+              class="form-control"
+              aria-label="Large"
+              placeholder="Buscar usuario"
+              @keypress.enter="haveResults = !haveResults"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-5 justify-content-center" v-if="haveResults">
+      <div class="card-deck w-75">
+        <div class="card">
+          <div class="card-img-top">
+            <h1>
+              <img class="w-100 justify-self-center" src="https://starrgate.s3.amazonaws.com:443/users/e33c47a328f9feeb11964aedf21be1ca3dca33ea/profile_idno8iE.jpg"/>
+            </h1>
+          </div>
+          <div class="card-body">
+            <h1>Hola</h1>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h1>Hola</h1>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h1>Hola</h1>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h1>Hola</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <div class="row justify-content-center" v-if="haveResults">
+      <div class="card w-75 bg-white">
+        <div class="card-body">
+          <div class="d-flex justify-content-end">
+            <h1>Pagination</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br/>
+    ,<br/>
+    <div class="row justify-content-center align-content-end">
+      <div class="container-fluid d-flex flex-wrap justify-content-center">
+        <div class="copyright d-flex flex-wrap">
+          <span>
+            &copy; Hecho por <a href="https://github.com/masuarez17">Marcos Suarez</a>&nbsp;
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      haveResults: false
+    }
   }
 }
 </script>
+
+<style scoped>
+</style>
